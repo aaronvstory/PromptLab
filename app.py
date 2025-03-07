cb069fa56dae2c70889a838881f9396b6bf4e273
Hruday Mouly <hruday96@users.noreply.huggingface.co>
2025-03-07 10:26:55 +0000

    Update app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import google.generativeai as genai
+
+# Streamlit app layout
+st.title('PromptLab')
+
+# Retrieve the API key from Streamlit secrets
+GOOGLE_API_KEY = st.secrets["GEMINI_API_KEY"]
+
+# Configure the Google Generative AI API with your API key
+genai.configure(api_key=GOOGLE_API_KEY)
+
+# Input field for the blog topic
+topic = st.text_area('Enter your promt:', placeholder='E.g., The Future of Artificial Intelligence')
