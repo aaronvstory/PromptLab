492300190028b3dcefcdd6114a903186679db75e
Hruday Mouly <hruday96@users.noreply.huggingface.co>
2025-03-07 19:51:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ st.title('PromptLab')
 # Create two columns for the Shinobi and Raikage buttons
 col1, col2 = st.columns(2)
 
-mode = st.radio("Choose a mode:", ["Shinobi", "Raikage"])
-st.write(f"You selected: {mode}")
+mode = st.radio("Choose a mode:", ["Shinobi", "Raikage"], horizontal=True)
 
 # Retrieve the API key from Streamlit secrets
 GOOGLE_API_KEY = st.secrets["GEMINI_API_KEY"]
@@ -18,3 +17,7 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 # Input field for the blog topic
 topic = st.text_area('Enter your prompt:')
+
+# Display selected mode
+st.write(f"You selected: {mode}")
+
